96116013192e93164932388c536139e7b7c83195
Radibadical <jpfowler@gmail.com>
2022-03-31 14:25:33 -0700

    fixed syntax

--- country-api.py
+++ country-api.py
@@ -5,20 +5,20 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = 'https://restcountries.eu/rest/v2/all'
-response = request.get(url)
+response = requests.get(url)
 print(response)
 print(response.status_code)
 
-countries = response.text
-data_python=json.loads(countries)
-country = input("enter country name, Capitalize the first letter:")
+# countries = response.text
+# data_python=json.loads(countries)
+# country = input("enter country name, Capitalize the first letter:")
 
-for i in range (len(data_python)):
-    if data_python[i]["name"] == country:
-        print("it is the number " +str(i)+ " in the countries list")
-        print("The population is " + str(data_python[i]["population"]))
-        break
-    else:
-        i+=1
-    if i == len(data_python):
-        print("no this country")
+# for i in range (len(data_python)):
+#     if data_python[i]["name"] == country:
+#         print("it is the number " +str(i)+ " in the countries list")
+#         print("The population is " + str(data_python[i]["population"]))
+#         break
+#     else:
+#         i+=1
+#     if i == len(data_python):
+#         print("no this country")
